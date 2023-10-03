087d1d65009e61b89458f12525041be22c7ceeab
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2023-10-03 20:57:17 +0300

    Lab finished

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import json
+from time import sleep
 
 
 HOST = '127.0.0.1'
@@ -12,44 +14,96 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 print(f'Connected to {HOST}:{PORT}')
 
+# Some client's paramters
+nickname = ''
+room_name = ''
+acknowledged = False
+
+
+# Call one time at the very beginning to setup nickname and the name of the room to join
+def initial_setup():
+    nickname = input("Pick a nickname visible to others - ")
+    room_name = input("Insert the name of the room to join/create - ")
+
+    message_data = {
+        'type': 'connect',
+        'payload': {
+            'nickname': nickname,
+            'room_name': room_name
+        }
+    }
+
+    client_socket.send(json.dumps(message_data).encode('utf-8'))
+
+# initial_setup()
 
 # Function to receive and display messages
 def receive_messages():
+    global acknowledged
+
     while True:
         try:
-            message = client_socket.recv(1024).decode('utf-8')
+            message = client_socket.recv(1024)
         
             if not message:
                 break
+
+            message_data = json.loads(message.decode('utf-8'))
         
-            # Print the recieved message
-            print(f'~\nReceived: {message}', "\nEnter a message (or 'exit' to quit): ", end='')
+            if message_data['type'] == 'connect_ack':
+                acknowledged = True
+                print(f"{message_data['payload']['message']}")
+            elif message_data['type'] == 'message':
+                print(f"~\n{message_data['payload']['sender']}: {message_data['payload']['text']}", "\nEnter a message (or 'exit' to quit): ", end='')
         except ConnectionAbortedError:
             print("Connection to the server was terminated ... :(")
             break
-
-
-# Function to send messages
-def send_messages():
-    while True:
-        message = input("Enter a message (or 'exit' to quit): ")
-    
-        if not message or message.lower() == 'exit':
-            break
-
-        # Send the message to the server
-        client_socket.send(message.encode('utf-8'))
-
 
 # Start the message reception thread
 receive_thread = threading.Thread(target=receive_messages)
 receive_thread.daemon = True
 receive_thread.start()
 
+
+# Function to send messages
+def send_chat_messages():
+    global acknowledged
+
+    while True:
+        if acknowledged == False:
+            nickname = input("Pick a nickname visible to others - ")
+            room_name = input("Insert the name of the room to join/create - ")
+
+            message_data = {
+                'type': 'connect',
+                'payload': {
+                    'nickname': nickname,
+                    'room_name': room_name
+                }
+            }
+        else:
+            message_text = input("Enter a message (or 'exit' to quit): ")
+    
+            if not message_text or message_text.lower() == 'exit':
+                break
+
+            message_data = {
+                'type': 'message',
+                'payload': {
+                    'sender': nickname,
+                    'room_name': room_name,
+                    'text': message_text
+                }
+            }
+
+        client_socket.send(json.dumps(message_data).encode('utf-8'))
+        sleep(1)
+
 # Start the message serving thread
-send_thread = threading.Thread(target=send_messages)
+send_thread = threading.Thread(target=send_chat_messages)
 send_thread.daemon = True
 send_thread.start()
+
 
 # Wait for both threads to complete before closing the client socket
 send_thread.join()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import json
 
 
 HOST = '127.0.0.1'
@@ -23,19 +24,44 @@ def handle_client(client_socket, client_address):
     print(f'Accepted connection from {client_address}')
 
     while True:
-        message = client_socket.recv(1024).decode('utf-8')
+        message = client_socket.recv(1024)
         
         if not message:
             break # Exit the loop when the client disconnects
 
-        print(f'Received from {client_address}: {message}')
-        
-        # Broadcast the message to all clients
-        for client in clients:
-            if client != client_socket: # find alternative to this IF
-                client.send(message.encode('utf-8'))
+        message_data = json.loads(message.decode('utf-8'))
 
-    # Remove the client from the list
+        print(f"Received from {client_address}: {message_data['type']}")
+
+        if message_data['type'] == 'connect':
+            if message_data['payload']['room_name'] not in rooms.keys():
+                rooms[message_data['payload']['room_name']] = []
+
+            rooms[message_data['payload']['room_name']].append(client_socket)
+
+            response_data = {
+                'type': 'connect_ack',
+                'payload': {
+                    'message': 'Successfully connected to the room!'
+                }
+            }
+
+            client_socket.send(json.dumps(response_data).encode('utf-8'))
+        # elif message_data['type'] == 'disconnect':
+        #     rooms[message_data['payload']['room_name']].remove(client_socket)
+        elif message_data['type'] == 'message':
+
+            # Broadcast the message to all the clients in the room
+            for client in rooms[message_data['payload']['room_name']]:
+                if client != client_socket:
+                    client.send(message)
+
+    # Remove the client from the list and room
+    for room in rooms.keys():
+        if client_socket in rooms[room]:
+            rooms[room].remove(client_socket)
+            break
+
     clients.remove(client_socket)
     client_socket.close()
 
